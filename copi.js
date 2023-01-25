44524262d842fb2f46b6863307efefca5c2e3570
josuisaigit config user.namegit statusgit config --local user.name josuisai <gherjos@gmail.com>
2023-01-25 11:16:54 -0600

    nuevo archivo de boton copiar

---
+++ copi.js
@@ -0,0 +1,10 @@
+
+
+  function copiar1(id_elemento) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+}
